2853629df1011c2294b5bba36049ecfb1ade8cb0
Iannn <pristianb007@gmail.com>
2024-02-24 13:58:39 +0700

    DOM Manipulation (2)

--- 3. DOM Manipulation (2)/JS/script.js
+++ 3. DOM Manipulation (2)/JS/script.js
@@ -6,11 +6,44 @@ title.innerHTML = "Hi Ian!";
 title.style.textAlign = "center";
 title.style.backgroundColor = "white";
 
-// Class List
-const first_section = document.querySelector("section#first-section");
-let text_2 = first_section.querySelector(".p-2");
-text_2.classList.toggle("active");
-console.log(text_2.classList.item(1));
-console.log(text_2.classList.contains("active"));
-// console.log(text_2.classList.replace("active", "inactive"));
-// console.log(text_2.classList);
+// createElement
+const newText = document.createElement("p");
+let newTextValue = document.createTextNode("I am new Text!");
+
+newText.appendChild(newTextValue);
+
+// Let's store this new element in a variable
+const first_section = document.querySelector("main section#first-section");
+first_section.appendChild(newText);
+
+// insertBefore
+const newLi = document.createElement("li");
+let newLiValue = document.createTextNode("I am new LI!");
+newLi.appendChild(newLiValue);
+
+// Let's store this to the li element
+const second_section_ul = document.querySelector("main section#second-section ul");
+const second_section_li_2 = second_section_ul.querySelector("li:nth-child(2)");
+second_section_ul.insertBefore(newLi, second_section_li_2);
+
+// removeChild
+let text_3 = document.querySelector("main section#first-section p.p-3");
+first_section.removeChild(text_3);
+
+// replaceChild
+const second_section = document.querySelector("main section#second-section");
+let text_4 = second_section.querySelector("p");
+
+let new_text_4_attribute = document.createElement("h1");
+let new_text_4_value = document.createTextNode("I am new text 4!");
+
+new_text_4_attribute.appendChild(new_text_4_value);
+
+second_section.replaceChild(new_text_4_attribute, text_4)
+
+text_4 = second_section.querySelector("h1");
+text_4.style.textAlign = "center";
+
+newText.style.backgroundColor = "lightgrey";
+newLi.style.backgroundColor = "lightgrey";
+text_4.style.backgroundColor = "lightgrey";
