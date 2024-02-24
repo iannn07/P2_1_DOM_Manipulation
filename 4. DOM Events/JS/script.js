6307006b307aa1059e0122e3938283adf2313a05
Iannn <pristianb007@gmail.com>
2024-02-24 14:23:16 +0700

    DOM Events DIR

---
+++ 4. DOM Events/JS/script.js
@@ -0,0 +1,7 @@
+// Just like the DOM Manipulation (2), I'll remove the previous script and make it to be default
+
+// getElementByID -> Element
+const title = document.getElementById("title");
+title.innerHTML = "Hi Ian!";
+title.style.textAlign = "center";
+title.style.backgroundColor = "white";
